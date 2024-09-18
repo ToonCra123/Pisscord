2809ca2e5c389881bbb15922aceb4541a9ab1a22
ToonCra123 <michaelguillory.mg@gmail.com>
2024-09-18 11:27:15 -0700

    Made the server work now

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,30 +1,35 @@
-import React, { useState, /*useEffect, useRef*/ } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { io, Socket } from 'socket.io-client';
 import './App.css';
 
-type MessageData = {
-  author: string,
-  message: string
+type Message = {
+  user: string;
+  message: string;
 };
 
-function App() {
-  const [messages, setMessages] = useState<string[]>([]);
-  const [newMessage, setNewMessage] = useState('');
-  // const ws = useRef<WebSocket | null>(null);
 
-  /*useEffect(() => {
-    ws.current = new WebSocket('ws://your-websocket-server-url');
-    ws.current.onmessage = (event) => {
-      setMessages((prevMessages) => [...prevMessages, event.data]);
-    };
+function App() {
+  const [username, setUsername] = useState('');
+  const [logged, setLogged] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [newMessage, setNewMessage] = useState('');
+  const socket = useRef<Socket | null>(null);
+
+  useEffect(() => {
+    socket.current = io('http://localhost:3000');
+    socket.current.on('message', (message: string) => {
+      let newMessage: Message = JSON.parse(message);
+      setMessages((prevMessages) => [...prevMessages, newMessage]);
+    });
     return () => {
-      ws.current?.close();
+      socket.current?.disconnect();
     };
-  }, []);*/
+  }, []);
 
   const sendMessage = () => {
     if (newMessage.trim() !== '') {
-      //ws.current?.send(newMessage);
-      setMessages((prevMessages) => [...prevMessages, newMessage]);
+      let sendNewMessage = JSON.stringify({ user: username, message: newMessage });
+      socket.current?.emit('message', sendNewMessage);
       setNewMessage('');
     }
   };
@@ -37,22 +42,37 @@ function App() {
 
   return (
     <>
-      <h1>Pisscord</h1>
-      <div className="chat-window">
-        <ul>
-          {messages.map((message, index) => (
-            <li key={index}>{message}</li>
-          ))}
-        </ul>
-      </div>
-      <input
-        type="text"
-        value={newMessage}
-        onChange={(e) => setNewMessage(e.target.value)}
-        onKeyPress={handleKeyPress}
-        placeholder="Type a message"
-      />
-      <button onClick={sendMessage}>Send</button>
+      { !logged ? 
+      <>
+        <h1>Pisscord</h1>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username"
+        />
+        <button onClick={() => setLogged(true)}>Login</button>
+      </> 
+      :
+      <>
+        <h1>Pisscord</h1>
+        <div className="chat-window">
+          <ul>
+            {messages.map((message, index) => (
+              <li key={index}>{message.user}: {message.message}</li>
+            ))}
+          </ul>
+        </div>
+        <input
+          type="text"
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+          onKeyPress={handleKeyPress}
+          placeholder="Type a message"
+        />
+        <button onClick={sendMessage}>Send</button>
+      </>
+      }
     </>
   );
 }
